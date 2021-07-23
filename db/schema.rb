2fd707d8e977a1aeb851ce1c4a825b3261d06b2b
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-24 03:44:10 +0800

    added default value for ave_rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_142648) do
+ActiveRecord::Schema.define(version: 2021_07_23_194118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2021_07_23_142648) do
     t.string "last_name"
     t.text "address"
     t.string "contact"
-    t.decimal "ave_rating"
+    t.decimal "ave_rating", default: "0.0"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
