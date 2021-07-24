8e4807d1d206f043059a45e5d87a269959bc18e8
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-24 10:13:38 +0800

    5 star rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,21 +14,21 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     # check first if review already exists
-    if params[:review][:user1_id].nil?                 # user1 is giving the review
-      @subject = User.find(params[:review][:user2_id])
-      review_check = Review.find_by(transact_id: params[:transact_id], user2_id: params[:review][:user2_id])
+    if params[:user1_id].nil?                 # user1 is giving the review
+      @subject = User.find(params[:user2_id])
+      review_check = Review.find_by(transact_id: params[:transact_id], user2_id: params[:user2_id])
       review_check&.delete
-      score = params[:review][:user2_score]
-    elsif params[:review][:user2_id].nil?              # user2 is giving the review
-      @subject = User.find(params[:review][:user1_id])
-      review_check = Review.find_by(transact_id: params[:transact_id], user1_id: params[:review][:user1_id])
+      score = params[:user2_score]
+    elsif params[:user2_id].nil?              # user2 is giving the review
+      @subject = User.find(params[:user1_id])
+      review_check = Review.find_by(transact_id: params[:transact_id], user1_id: params[:user1_id])
       review_check&.delete
-      score = params[:review][:user1_score]
+      score = params[:user1_score]
     end
 
     if @review.save
       # update subject ave_rating
-      # score = (params[:review][:user1_score] || params[:review][:user2_score])
+      # score = (params[:user1_score] || params[:user2_score])
       @subject.update_rating(score.to_i)
       redirect_to root_path, notice: 'Review was submitted succesfully.'
     else
@@ -39,6 +39,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:transact_id, :user1_id, :user2_id, :user1_score, :user2_score, :description, :traded_with)
+    params.permit(:transact_id, :user1_id, :user2_id, :user1_score, :user2_score, :description, :traded_with)
   end
 end
